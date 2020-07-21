8c0a6eb9003f8942b758e23619b88d53b40d88a4
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-21 13:08:52 +0300

    redux principles

--- src/redux/destructure.js
+++ src/redux/destructure.js
@@ -11,4 +11,6 @@ console.log(person.profession); // surfer
 var { city, profession } = person;
 
 console.log(profession); // surfer
+
+//we can console.log(prefession) to access surfer
 var person = { profession }
