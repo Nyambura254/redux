cf5d9e8a07c46ba30387bebf24578639fc39e0c8
BonnieNyambura <nyamburabonnie@gmail.com>
2020-07-22 11:50:44 +0300

    action creator

--- src/redux/finance.js
+++ src/redux/finance.js
@@ -42,21 +42,85 @@ const removeExpense = (payload = {}) => { //payload is a piece of information se
     };
 };
 
+// // expenses reducer
+// var expensesReducerDefaultState = []; //initial state is empty array
+// var expensesReducer = (state = expensesReducerDefaultState, action) => {
+//     switch (action.type) { //action must have a type to show what to be performed
+//         case "ADD_EXPENSE":
+//             return [...state, action.expense];
+//         case "REMOVE_EXPENSE":
+//             return state.filter((expense) => expense.id !== action.id); //filter expense by id,if no action.id
+//         default:
+//             return state; //default value-initial state
+//     }
+// };
+const editExpense = (id, updates) => {
+    return {
+        type: "EDIT_EXPENSE",
+        id,
+        updates,
+    };
+};
+
+// filters action creators
+var setTextFilter = (text = "") => {
+    return {
+        type: "SET_TEXT_FILTER",
+        text,
+    };
+};
+
+var sortByAmount = () => {
+    return {
+        type: "SORT_BY_AMOUNT",
+    };
+};
+var sortByDate = () => {
+    return {
+        type: "SORT_BY_DATE",
+    };
+};
+
+var setStartDate = (startDate) => {
+    return {
+        type: "SET_START_DATE",
+        startDate,
+    };
+};
+var setEndDate = (endDate) => {
+    return {
+        type: "SET_END_DATE",
+        endDate,
+    };
+};
+
 // expenses reducer
-var expensesReducerDefaultState = []; //initial state is empty array
+var expensesReducerDefaultState = [];
 var expensesReducer = (state = expensesReducerDefaultState, action) => {
-    switch (action.type) { //action must have a type to show what to be performed
+    switch (action.type) {
         case "ADD_EXPENSE":
             return [...state, action.expense];
         case "REMOVE_EXPENSE":
-            return state.filter((expense) => expense.id !== action.id); //filter expense by id,if no action.id
+            return state.filter((expense) => expense.id !== action.id);
+        case "EDIT_EXPENSE":
+            return state.map((expense) => {
+                if (expense.id === action.id) {
+                    // return {
+                    //   ...expense,
+                    //   ...action.updates,
+                    // };
+                    return Object.assign({}, expense, action.updates);
+                } else {
+                    return expense;
+                }
+            });
         default:
-            return state; //default value-initial state
+            return state;
     }
 };
-
 // [].concat('joseph') --- ['joseph'] // [].concat({ description: "Electric Bills", amount: 200 })
 
+// 
 // filters reducer
 var filtersReducerDefaultState = {
     text: "",
@@ -66,31 +130,62 @@ var filtersReducerDefaultState = {
 };
 var filtersReducer = (state = filtersReducerDefaultState, action) => {
     switch (action.type) {
-        default: return state;
+        case "SET_TEXT_FILTER":
+            return Object.assign({}, state, { text: action.text });
+        case "SORT_BY_AMOUNT":
+            return Object.assign({}, state, { sortBy: "amount" });
+        case "SORT_BY_DATE":
+            return Object.assign({}, state, { sortBy: "date" });
+        case "SET_START_DATE":
+            return Object.assign({}, state, { startDate: action.startDate });
+        case "SET_END_DATE":
+            return Object.assign({}, state, { endDate: action.endDate });
+        default:
+            return state;
     }
 };
 
+// you guys are REDUX NINJAS(TRAINEE ninjas)
 
-//in redux we must have only and only one store where we can reference our value by either calling them or by querry
+// selectors(querying the redux state)
+var visibleExpenses = (expenses, filters) => {
+    return expenses.filter((expense) => {
+        var textMatch = expense.description
+            .toLowerCase()
+            .includes(filters.text.toLowerCase());
+        return textMatch;
+    });
+};
+
 var store = createStore(
-    combineReducers({ //we use combineReducer for all our reducer
+    combineReducers({
         expenses: expensesReducer,
         filters: filtersReducer,
     })
 );
-//detects changes and logs it on console
+
 store.subscribe(() => {
-    console.log(store.getState()); //current store
+    var state = store.getState();
+    var showExpenses = visibleExpenses(state.expenses, state.filters);
+    console.log(showExpenses);
 });
 
-//addExpenseOne
 var expenseOne = store.dispatch(
     addExpense({ description: "Electric Bills", amount: 200 })
 );
-
-//addExpenzeTwo
 var expenseTwo = store.dispatch(
-    addExpense({ description: "Hot Coffee", amount: 500 })
+    addExpense({ description: "Reducer in hooks", amount: 500 })
 );
 
-store.dispatch(removeExpense({ id: expenseOne.expense.id })); //when we want to remove...we can target by id on addExpenseTwo what we have already in our store
+// store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+
+store.dispatch(
+    editExpense(expenseTwo.expense.id, {
+        description: "trip to Manchester,UK",
+        amount: 1000,
+    })
+);
+
+store.dispatch(setTextFilter("el"));
+
+// london, barcelona (elo) --> barcelona
