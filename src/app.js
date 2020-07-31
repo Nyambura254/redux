beae3cffd340a1eea18154182d10047ebcf72d90
Boniface Mwangi <45118623+Nyambura254@users.noreply.github.com>
2020-07-31 08:52:32 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,5 +1,5 @@
 import React from "react";// import react to enable rendring
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom";// to enable reactDOM
 import TodoApp from "./components/TodoApp";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
